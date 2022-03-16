a89a5bfcb0d5080f62a4c1acbff1e14bd7027539
SeymurMs <99805897+SeymurMs@users.noreply.github.com>
2022-03-16 16:50:32 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Group group = new Group();
+            group.No = "AP203";
+            Console.WriteLine(group.No);
+        }
+    }
+}
